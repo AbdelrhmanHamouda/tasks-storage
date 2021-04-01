a26594ab0132a4624c3a221c1a446dec25009dfc
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-04-01 20:47:13 +0200

    Update return types

--- src/main/java/com/pomos/interfaces/TaskRepository.java
+++ src/main/java/com/pomos/interfaces/TaskRepository.java
@@ -3,7 +3,6 @@ package com.pomos.interfaces;
 import com.pomos.tables.Task;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,17 +10,17 @@ public interface TaskRepository {
 
     Optional<Task> findById(@NotNull Long id);
 
-    Task save(@NotNull String summary, @NotNull LocalDateTime dateCreated, String priority);
+    Task save(@NotNull String summary, String priority);
 
-    Task save(@NotNull String summary, @NotNull LocalDateTime dateCreated);
+    Task save(@NotNull String summary);
 
     Long deleteTask(@NotNull Long id);
 
-    Task completeTask(@NotNull Long id);
+    int completeTask(@NotNull Long id);
 
-    Task updatePriority(@NotNull Long id, @NotNull String priority);
+    int updatePriority(@NotNull Long id, @NotNull String priority);
 
-    Task updateSummary(@NotNull Long id, @NotNull String summary);
+    int updateSummary(@NotNull Long id, @NotNull String summary);
 
     List<Task> listAllTasks();
 }
