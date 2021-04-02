d00703bbd3472113ad8d041d2bab4c03fe59c22d
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-04-02 19:29:13 +0200

    Implementation of the URI utils

--- src/main/java/com/pomos/utils/UriUtils.java
+++ src/main/java/com/pomos/utils/UriUtils.java
@@ -1,4 +1,22 @@
 package com.pomos.utils;
 
+import com.pomos.tables.Task;
+
+import java.net.URI;
+
 public class UriUtils {
+
+    /**
+     * Method to create a new path for any newly created task
+     *
+     * @param id of the task to create the URI for.
+     * @return created URI location
+     */
+    public static URI location(Long id) {
+        return URI.create("/tasks/" + id);
+    }
+
+    public static URI location(Task task) {
+        return location(task.getId());
+    }
 }
