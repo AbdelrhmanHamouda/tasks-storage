6a35235ef0be70c5cceb57b4a62d8f4298874cb9
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-04-02 12:00:43 +0200

    Implement TaskUpdateSummaryCommand

--- src/main/java/com/pomos/modules/TaskUpdateSummaryCommand.java
+++ src/main/java/com/pomos/modules/TaskUpdateSummaryCommand.java
@@ -1,4 +1,20 @@
 package com.pomos.modules;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TaskUpdateSummaryCommand {
+    @NotNull
+    private Long id;
+    @NotBlank
+    private String summary;
 }
